c53d8cf2aebd978b0d8aa4eb5b054a89b8829ea7
Felipe Segura <fsegurar@unbosque.edu.co>
2022-03-05 16:45:16 -0500

    felipe's work

--- src/co/edu/unbosque/ManejoArchivo.java
+++ src/co/edu/unbosque/ManejoArchivo.java
@@ -1,7 +1,10 @@
 package co.edu.unbosque;
 
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 
@@ -14,8 +17,12 @@ import com.opencsv.exceptions.CsvValidationException;
 public class ManejoArchivo {
 	
 	private  ArrayList<Encuentros> encuentros;
+	private  ArrayList<Clientes> clientes;
+	private  ArrayList<Apuestas> apuestas;
 	private  CSVReader csvReader;
 	private  FileReader csvFile;
+	private int posicionC;
+	private int posicionE;
 
 	
 	public ManejoArchivo() {
@@ -56,5 +63,172 @@ public class ManejoArchivo {
 		return resul;
 		
 	}
+
+	public void  leerMaeclientes(String archivo) {
+
+		try {
+			csvFile = new FileReader(archivo);
+			CSVParser conPuntoYComa = new CSVParserBuilder().withSeparator(';').build();
+			csvReader = new CSVReaderBuilder(csvFile).withCSVParser(conPuntoYComa).build();
+			String[] fila = null;
+			csvReader.readNext();
+			while ((fila = csvReader.readNext()) != null) {
+				try {
+					
+					clientes.add(new Clientes(Integer.parseInt(fila[0]), fila[1], Integer.parseInt(fila[2])));
+
+				} catch (EmptyStackException e) {
+					
+				} catch (NumberFormatException e) {
+					
+				}
+			}
+			csvFile.close();
+			csvReader.close();
+
+		} catch (IOException e) {
+			
+		} catch (CsvValidationException e) {
+
+		}
+		
+	}
+
+	public void  leerApuestas(String archivo) {
+
+		try {
+			csvFile = new FileReader(archivo);
+			CSVParser conPuntoYComa = new CSVParserBuilder().withSeparator(';').build();
+			csvReader = new CSVReaderBuilder(csvFile).withCSVParser(conPuntoYComa).build();
+			String[] fila = null;
+			csvReader.readNext();
+			while ((fila = csvReader.readNext()) != null) {
+				try {
+					
+					apuestas.add(new Apuestas(Integer.parseInt(fila[0]), Integer.parseInt(fila[1]), Integer.parseInt(fila[2]),Integer.parseInt(fila[3])));
+
+				} catch (EmptyStackException e) {
+					
+				} catch (NumberFormatException e) {
+					
+				}
+			}
+			csvFile.close();
+			csvReader.close();
+
+		} catch (IOException e) {
+			
+		} catch (CsvValidationException e) {
+
+		}
+		
+	}
+	
+	public String escribirApuestas(String archivo) {
+		File f = new File(archivo);
+		try {
+			FileWriter fw = new FileWriter(f);
+			PrintWriter pw = new PrintWriter(fw); 
+			String datosString = "";
+			for(int i = 0;i<apuestas.size();i++) {
+				int consecutivo = apuestas.size();
+				int idEncuentro = apuestas.get(i).getIdEncuentro();
+				int valorApostado = apuestas.get(i).getValorApostado();
+				int idUsuario = apuestas.get(i).getIdUsuario();
+				datosString = consecutivo+";"+idEncuentro+";"+valorApostado+";"+idUsuario;
+			
+				pw.println(datosString); 
+			}
+			fw.close();
+		} catch (IOException e) {
+			return "Registro no exitoso";
+		}
+		return "Registro exitoso";
+	}
+
+	public boolean encontrarCliente(int id){
+		boolean respuesta=false;
+		for(int i =0;i<clientes.size();i++){
+			if(id==clientes.get(i).getId()){
+				posicionC=i;
+				respuesta=true;
+			}else{
+				respuesta=false;
+			}
+		}
+		return respuesta;
+	}
+
+	public boolean encontrarEncuentro(int id){
+		boolean respuesta=false;
+		for(int i =0;i<encuentros.size();i++){
+			if(id==encuentros.get(i).getConsecutivo()){
+				posicionE=i;
+				respuesta=true;
+			}else{
+				respuesta=false;
+			}
+		}
+		return respuesta;
+	}
+
+	public ArrayList<Encuentros> getEncuentros() {
+		return encuentros;
+	}
+
+	
+	public int getPosicionC() {
+		return posicionC;
+	}
+
+	public void setPosicionC(int posicionC) {
+		this.posicionC = posicionC;
+	}
+
+	public int getPosicionE() {
+		return posicionE;
+	}
+
+	public void setPosicionE(int posicionE) {
+		this.posicionE = posicionE;
+	}
+
+	public ArrayList<Apuestas> getApuestas() {
+		return apuestas;
+	}
+
+	public void setApuestas(ArrayList<Apuestas> apuestas) {
+		this.apuestas = apuestas;
+	}
+
+	public void setEncuentros(ArrayList<Encuentros> encuentros) {
+		this.encuentros = encuentros;
+	}
+
+	public ArrayList<Clientes> getClientes() {
+		return clientes;
+	}
+
+	public void setClientes(ArrayList<Clientes> clientes) {
+		this.clientes = clientes;
+	}
+
+	public CSVReader getCsvReader() {
+		return csvReader;
+	}
+
+	public void setCsvReader(CSVReader csvReader) {
+		this.csvReader = csvReader;
+	}
+
+	public FileReader getCsvFile() {
+		return csvFile;
+	}
+
+	public void setCsvFile(FileReader csvFile) {
+		this.csvFile = csvFile;
+	}
+	
+		
 
 }
--- src/co/edu/unbosque/MotorApuestas.java
+++ src/co/edu/unbosque/MotorApuestas.java
@@ -13,6 +13,7 @@ public class MotorApuestas implements Runnable {
     public MotorApuestas(Socket socket) {
         this.socket = socket;
         manejoArchivo = new ManejoArchivo();
+        manejoArchivo.leerMaeclientes("Maeclientes.csv");
     }
 
     @Override
@@ -25,15 +26,55 @@ public class MotorApuestas implements Runnable {
             var out = new PrintWriter(socket.getOutputStream(), true);
 
             while (in.hasNextLine()) {
-                var id = in.nextLine();
-                safePrintln("ID recibido: " + id);
+                boolean flag=false;
+                do{
+                    var id = in.nextLine();
+                    safePrintln("ID recibido: " + id);
+                     int idC = Integer.parseInt(id);
+                    if(manejoArchivo.encontrarCliente(idC)) {
+                        flag=true;
+                        String encuentros = "Lista de encuentros\n"+manejoArchivo.leerEncuentros("Encuentros.csv");
+                        out.println("Ingreso completado\n"+encuentros);
+                        safePrintln("Ingresï¿½ el usuario: " + id);
+                        boolean falgEncuentro=false;
+                        do{
+                            out.println("ingrese el Id del encuentro por el que desea apostar");
+                            var consecutivo = in.nextLine();
+                            safePrintln("Consecutivo: " + consecutivo);
+                            int idEn=Integer.parseInt(consecutivo) ;
+                            if(manejoArchivo.encontrarEncuentro(idEn)){
+                                falgEncuentro=true;
+                                boolean flagValor=false;
+                                do{
+                                    out.println("ingrese el valor que desea apostar");
+                                    var valor = in.nextLine();
+                                    safePrintln("Consecutivo: " + consecutivo);
+                                    int valorapuesta=Integer.parseInt(valor);
+                                    if(valorapuesta<manejoArchivo.getClientes().get(manejoArchivo.getPosicionC()).getSaldo()){
+                                        flagValor=true;
+                                        manejoArchivo.getApuestas().add(new Apuestas(manejoArchivo.getApuestas().size()+1,idEn,valorapuesta,idC));
+                                        manejoArchivo.escribirApuestas("Apuestas.csv");
+                                        out.println("Resumen de la transaccion");
+                                        out.println(manejoArchivo.getClientes().get(manejoArchivo.getPosicionC()).getNombre());
+                                        out.println("Saldo inicial: "+manejoArchivo.getClientes().get(manejoArchivo.getPosicionC()).getSaldo());
+                                        out.println("Valor apostado: "+valor);
+                                        out.println("Encuentro : \nDeporte :"+manejoArchivo.getEncuentros().get(manejoArchivo.getPosicionE()).getDeporte()+"\nEquipo local: "+manejoArchivo.getEncuentros().get(manejoArchivo.getPosicionE()).getEquipoLocal()+" vs "+manejoArchivo.getEncuentros().get(manejoArchivo.getPosicionE()).getEquipoVisitante());
+                                        out.println("Nuevo Saldo : "+(manejoArchivo.getClientes().get(manejoArchivo.getPosicionC()).getSaldo()-valorapuesta));
+                                    }else{
+                                        out.println("Lo sentimos su el valor de su apuesta es mayor que su saldo\nSus saldo es de "+manejoArchivo.getClientes().get(manejoArchivo.getPosicionC()).getSaldo());
+                                    }
+                                }while(flagValor=false);
+                            }else {
+                                out.println("Por favor ingrese un id del encuentro valido");
+                            }
+                        }while(falgEncuentro=false);
+
+                        
+                    }else  {
+                        out.println("Cliente no encontrado por favor intente nuevamente");
+                    }
+                }while(flag=false);
                 
-                //Ejemplo de operación
-                if(id.equals("001")) {
-                    String encuentros = "Lista de encuentros\n"+manejoArchivo.leerEncuentros("Encuentros.csv");
-                	out.println("Ingreso completado\n"+encuentros);
-                    safePrintln("Ingresó el usuario: " + id);
-                }
                 
             }
         } catch (Exception e) {
@@ -42,7 +83,7 @@ public class MotorApuestas implements Runnable {
             try {
                 socket.close();
             } catch (IOException e) {}
-            safePrintln("Se cerró el Socket: " + socket);
+            safePrintln("Se cerrï¿½ el Socket: " + socket);
         }
     }
 
